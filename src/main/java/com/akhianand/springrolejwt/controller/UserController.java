40afb3b84f3cf88be37d4893daff508988523c21
lasvistass <spinelli1k90@gmail.com>
2021-12-06 22:42:25 +0100

    jwt ok

--- src/main/java/com/akhianand/springrolejwt/controller/UserController.java
+++ src/main/java/com/akhianand/springrolejwt/controller/UserController.java
@@ -23,46 +23,40 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private TokenProvider jwtTokenUtil;
+	@Autowired
+	private TokenProvider jwtTokenUtil;
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-    public ResponseEntity<?> generateToken(@RequestBody LoginUser loginUser) throws AuthenticationException {
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> generateToken(@RequestBody LoginUser loginUser) throws AuthenticationException {
 
-        final Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginUser.getUsername(),
-                        loginUser.getPassword()
-                )
-        );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        final String token = jwtTokenUtil.generateToken(authentication);
-        return ResponseEntity.ok(new AuthToken(token));
-    }
+		final Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginUser.getUsername(), loginUser.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		final String token = jwtTokenUtil.generateToken(authentication);
+		return ResponseEntity.ok(new AuthToken(token));
+	}
 
-    @RequestMapping(value="/register", method = RequestMethod.POST)
-    public User saveUser(@RequestBody UserDto user){
-        return userService.save(user);
-    }
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public User saveUser(@RequestBody UserDto user) {
+		return userService.save(user);
+	}
 
+	@PreAuthorize("hasRole('ADMIN')")
+	@RequestMapping(value = "/adminping", method = RequestMethod.GET)
+	public String adminPing() {
+		return "Only Admins Can Read This";
+	}
 
-
-    @PreAuthorize("hasRole('ADMIN')")
-    @RequestMapping(value="/adminping", method = RequestMethod.GET)
-    public String adminPing(){
-        return "Only Admins Can Read This";
-    }
-
-    @PreAuthorize("hasRole('USER')")
-    @RequestMapping(value="/userping", method = RequestMethod.GET)
-    public String userPing(){
-        return "Any User Can Read This";
-    }
+	@PreAuthorize("hasRole('USER')")
+	@RequestMapping(value = "/userping", method = RequestMethod.GET)
+	public String userPing() {
+		return "Any User Can Read This";
+	}
 
 }
